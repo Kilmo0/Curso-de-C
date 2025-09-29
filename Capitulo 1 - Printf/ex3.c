2fcbcd25fcbcbc0222e30d85a3573c3fc9dbce24
Kilmo0 <lorenzikaua1@gmail.com>
2025-09-29 16:26:14 -0300

    V1.0

---
+++ Capitulo 1 - Printf/ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("1 - Clientes\n");
+    printf("2 - fornecedores\n");
+    printf("3 - Faturas\n\n\n");
+    printf("0 - Sair\n");
+    getchar();
+
+    return 0;
+}
---
+++ Capitulo 1 - Printf/ex4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Aqui vai um som de Apito\a\n");
+    getchar();
+    printf("aqui vai um som de apito\7\n");
+    getchar();
+    return 0;
+}
